5b649ec8692a9ba7d9636ee72bf81c1c06d2234f
cblech <cblech@giggedigithub>
2019-12-26 02:51:30 +0100

    Routing test updeate

--- web-server/Routing.cpp
+++ web-server/Routing.cpp
@@ -12,13 +12,14 @@ Routing::Routing()
 	addRoute(new Route("/bootstrap.css.map", StaticResources::bootstrap_css_map));
 	addRoute(new Route("/pic.png", StaticResources::Pic_png));
 	addRoute(new Route("/favicon.ico", StaticResources::caswaf_png));
+	addRoute(new Route("/blabla", StaticResources::caswaf_png));
 	addRoute(new Route("/huhu", StaticResources::fu_txt));
 
 	//html Routes
 	addRoute(new Route("/", new RootController()));
-	addRoute(new Route("/one", new RootController()));
+	addRoute(new Route("/one/*", new RootController()));
 	addRoute(new Route("/one/one", new RootController()));
-	addRoute(new Route("/two", new RootController()));
+	addRoute(new Route("/two/", new RootController()));
 	addRoute(new Route("/two/*/bobo", new RootController()));
 
 }
